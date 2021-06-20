0db918c808633581fd8bdcce352e4e5a50d6fd37
gibers <chbaptis@hotmail.fr>
2021-06-20 08:14:56 +0200

    test git connect with ssh-key

--- src/test/java/com/example/bern/accessRessourceInTest/testJUnit/TestAccessRessourceInTest.java
+++ src/test/java/com/example/bern/accessRessourceInTest/testJUnit/TestAccessRessourceInTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+
 @SpringJUnitConfig(value = Config1.class)
 public class TestAccessRessourceInTest {
 
